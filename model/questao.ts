ce218328af9e0788625542102f62e556551bf53e
David Pires <developer.david@outlook.com>
2022-03-06 12:39:10 -0300

    Classes resposta.ts and questao.ts Created

---
+++ model/questao.ts
@@ -0,0 +1,37 @@
+import RespostaModel from './resposta';
+
+export default class QuestaoModel {
+  #id: number;
+  #enunciado: string;
+  #respostas: RespostaModel[];
+  #acertou: boolean;
+  // #respondida: boolean
+
+  contructor(id: number, enunciado:string, respostas: RespostaModel[], acertou = false) {
+    this.#id = id;
+    this.#enunciado = enunciado;
+    this.#respostas = respostas;
+    this.#acertou = acertou;
+  }
+
+  get id() {
+    return this.#id
+  }
+  get enunciado() {
+    return this.#enunciado
+  }
+
+  get respostas() {
+    return this.#respostas
+  }
+
+  get acertou() {
+    return this.#acertou
+  }
+  get respondida() {
+    for(let resposta of this.#respostas) {
+      if(resposta.revelada) return true
+    }
+    return false
+  }
+}
