e26fcdcb602f87d1a1219dcffa9ba8abc3dbf513
David Pires <developer.david@outlook.com>
2022-03-09 01:48:07 -0300

    conexão with api and questions database

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -1,5 +1,15 @@
 import questoes from '../bancoDeQuestoes'
 export default (req, res) => {
-  
+  const idSelecionado = +req.query.id
+
+  const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+
+  if(unicaQuestaoOuNada.length === 1) {
+    const questaoSelecionada = unicaQuestaoOuNada[0]
+    res.status(200).json(questaoSelecionada.paraObjeto())
+  } else {
+    res.status(204).send()
+  }
+
   res.status(200).json(questoes[0].paraObjeto())
 }
