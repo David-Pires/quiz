43d503b2343f58a6c4e7cf7f3cf23329c4fe8cf9
David Pires <developer.david@outlook.com>
2022-03-17 16:47:17 -0300

    component Resposta.tsx created

---
+++ components/Resposta.tsx
@@ -0,0 +1,16 @@
+import RespostaModel from '../model/resposta'
+
+interface RespostaProps {
+  valor: RespostaModel
+  indice: number
+  letra: string
+  corLetra: string
+}
+
+export default function Resposta(props) {
+  return (
+    <div>
+
+    </div>
+  )
+}
