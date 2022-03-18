f3d5e0bcd332dfdc1277778a6e6860143d2a744d
David Pires <developer.david@outlook.com>
2022-03-18 17:16:31 -0300

    key reference to be fixed

--- components/Questao.tsx
+++ components/Questao.tsx
@@ -1,35 +1,33 @@
 /* eslint-disable react/jsx-key */
-import styles from '../styles/Questao.module.css'
-import QuestaoModel from '../model/questao'
-import Enunciado from './Enunciado'
-import Resposta from './Resposta'
+import styles from "../styles/Questao.module.css";
+import QuestaoModel from "../model/questao";
+import Enunciado from "./Enunciado";
+import Resposta from "./Resposta";
 
+interface QuestaoProps {
+  valor: QuestaoModel;
+}
 
-  interface QuestaoProps {
-    valor: QuestaoModel
+export default function Questao(props: QuestaoProps) {
+  const questao = props.valor;
+
+  function renderizarRespostas() {
+    return questao.respostas.map((resposta, i) => {
+      return (
+        <Resposta
+        valor={resposta}
+        indice={i}
+        letra="A"
+        corLetra="#F2C866"
+        />
+      );
+    });
   }
 
-
-  export default function Questao(props: QuestaoProps) {
-    const questao = props.valor
-
-    function renderizarRespostas() {
-      return questao.respostas.map((resposta, i) => {
-        return ( 
-        <Resposta 
-            valor={resposta}
-            indice={i}            
-            letra="A"
-            corLetra="#F2C866"
-          />
-        )
-    })
-  }
-  
   return (
     <div className={styles.questao}>
-      <Enunciado texto={questao.enunciado}/>
+      <Enunciado texto={questao.enunciado} />
       {renderizarRespostas()}
     </div>
-  )
+  );
 }
