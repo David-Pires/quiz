5a9734b1c2bc3b37e6ac0fd8d0c0b4bcf6187057
David Pires <developer.david@outlook.com>
2022-03-23 14:01:38 -0300

    adjusting layout and design of questions

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -27,22 +27,20 @@ export default function Resposta(props: RespostaProps) {
           </div> */}
           <div className={styles.verso}>
             {resposta.certa ? (
-
+              <div className={styles.certa}>
+                  <div>A resposta certa é...</div>
+                  <div className={styles.valor}>
+                    {resposta.valor}
+                  </div>
+              </div>
             ) : (
-
-            )}
-            <div className={styles.certa}>
-                <div>A resposta certa é...</div>
-                <div className={styles.valor}>
-                  {resposta.valor}
-                </div>
-            </div>
             <div className={styles.errada}>
                 <div>A resposta informada está errada...</div>
                 <div className={styles.valor}>
                   {resposta.valor}
                 </div>
             </div>
+            )}
           </div>
         </div>
       </div>
