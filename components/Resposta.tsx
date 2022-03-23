fc2fb729f2909db6024b69294776f555bb71ee22
David Pires <developer.david@outlook.com>
2022-03-23 14:15:25 -0300

    fixed div

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -25,29 +25,31 @@ export default function Resposta(props: RespostaProps) {
           <div className={styles.valor}>
             {resposta.valor}
           </div>
+        </div>
+
         ) : (
 
-        )}
-
-          <div className={styles.verso}>
-            {resposta.certa ? (
-              <div className={styles.certa}>
-                  <div>A resposta certa é...</div>
+            <div className={styles.verso}>
+              {resposta.certa ? (
+                <div className={styles.certa}>
+                    <div>A resposta certa é...</div>
+                    <div className={styles.valor}>
+                      {resposta.valor}
+                    </div>
+                </div>
+              ) : (
+              <div className={styles.errada}>
+                  <div>A resposta informada está errada...</div>
                   <div className={styles.valor}>
                     {resposta.valor}
                   </div>
               </div>
-            ) : (
-            <div className={styles.errada}>
-                <div>A resposta informada está errada...</div>
-                <div className={styles.valor}>
-                  {resposta.valor}
-                </div>
+              )}
             </div>
-            )}
-          </div>
+          )}
         </div>
       </div>
+    )
+  }
+
     
-  )
-}
