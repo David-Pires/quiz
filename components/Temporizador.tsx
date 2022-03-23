6b1d270fb39e0398a611e05ca4f68ed61a115f77
David Pires <developer.david@outlook.com>
2022-03-23 15:29:50 -0300

    Temporizador.module.css

--- components/Temporizador.tsx
+++ components/Temporizador.tsx
@@ -1,3 +1,6 @@
+import styles from '../styles/Temporizador.module.css' 
+
+   
 interface TemporizadorProps {
   duracao: number
   tempoEsgostado: () => void
@@ -5,7 +8,7 @@ interface TemporizadorProps {
 export default function Temporarizador(props) {
   return (
     <div>
-
+      <div className={styles.temporizador}></div>
     </div>
   )
 }
