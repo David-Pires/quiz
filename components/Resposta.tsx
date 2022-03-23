d6805a16b98feddecf31bda2116ef57e2fb67348
David Pires <developer.david@outlook.com>
2022-03-23 13:14:57 -0300

    expression for right and wrong answer

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -26,13 +26,23 @@ export default function Resposta(props: RespostaProps) {
             {resposta.valor}
           </div> */}
           <div className={styles.verso}>
+            {resposta.certa ? (
+
+            ) : (
+
+            )}
             <div className={styles.certa}>
                 <div>A resposta certa é...</div>
                 <div className={styles.valor}>
                   {resposta.valor}
                 </div>
             </div>
-
+            <div className={styles.errada}>
+                <div>A resposta informada está errada...</div>
+                <div className={styles.valor}>
+                  {resposta.valor}
+                </div>
+            </div>
           </div>
         </div>
       </div>
