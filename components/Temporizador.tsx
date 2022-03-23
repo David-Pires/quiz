9e5fef6f762d973159924b5d5e3b79cacda12c64
David Pires <developer.david@outlook.com>
2022-03-23 15:37:43 -0300

    Properties of Temporizador.tsx by an array

--- components/Temporizador.tsx
+++ components/Temporizador.tsx
@@ -1,14 +1,26 @@
 import styles from '../styles/Temporizador.module.css' 
-
+import { CountdownCircleTimer} from 'react-countdown-circle-timer'
    
 interface TemporizadorProps {
   duracao: number
   tempoEsgostado: () => void
 }
-export default function Temporarizador(props) {
+export default function Temporarizador(props: TemporizadorProps) {
   return (
     <div>
-      <div className={styles.temporizador}></div>
+      <div className={styles.temporizador}>
+        <CountdownCircleTimer
+        duration={props.duracao}
+          size={120}
+          isPlaying
+          onComplete={props.tempoEsgostado}
+          colors={[
+            ['#bce596', 0.33],
+            ['#f7b801', 0.33],
+            ['#ed827a', 0.33],
+          ]}
+        />
+      </div>
     </div>
   )
 }
