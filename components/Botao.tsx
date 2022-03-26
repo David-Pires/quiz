bdedfe5c866c57bbebbae124c048ffc035f078be
David Pires <developer.david@outlook.com>
2022-03-26 07:53:41 -0300

    created new component Botao.tsx and Botao.module.css for styling

---
+++ components/Botao.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Botao.module.css'
+
+interface BotaoProps {
+  href?: string
+  texto: string
+  onClick?: (e: any) => void
+}
+export default function Botao(prop: BotaoProps) {
+  return (
+    <button>
+
+    </button>
+  )
+}
