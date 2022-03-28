7cfd8519b708487bccfd847fd00cf5d30fd4ee28
David Pires <developer.david@outlook.com>
2022-03-28 00:34:03 -0300

    function questaoRespondida(questaoRespondida: QuestaoModel)

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ const BASE_URL = 'http://localhost:3000/api'
 export default function Home() {
   const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
   const [questao, setQuestao] = useState<QuestaoModel>(questaoMock)
+  const [respostasCertas, setRespostasCertas] = useState<number>(0)
 
   async function carregarIdsDasQuestoes(){ 
     const resp = await fetch(`${BASE_URL}/questionario`)
@@ -39,12 +40,14 @@ export default function Home() {
     idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
   }, [idsDasQuestoes])
   
-  function questaoRespondida(questao: QuestaoModel) {
-
+  function questaoRespondida(questaoRespondida: QuestaoModel) {
+    setQuestao(questaoRespondida)
+    const acertou = questaoRespondida.acertou
+    setRespostasCertas(respostasCertas + (acertou ? 1 : 0))
   }
 
   function irProProximoPasso() {
-
+    
   }
     
 
