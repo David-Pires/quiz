80875f7990bbf499d62a71c339c54f00b78af5e7
David Pires <developer.david@outlook.com>
2022-03-28 23:37:52 -0300

    refactoring for production

--- components/Botao.tsx
+++ components/Botao.tsx
@@ -1,27 +1,27 @@
-import Link from 'next/link'
 import styles from '../styles/Botao.module.css'
+import Link from 'next/link'
 
 interface BotaoProps {
-  texto: string
-  href?: string
-  onClick?: (e: any) => void
+    texto: string
+    href?: string
+    onClick?: (e: any) => void
 }
+
+
 export default function Botao(props: BotaoProps) {
 
-  function renderizarBotao( ) {
-    return (
-      <button className={styles.botao}>
-          {props.onClick}
-          {props.texto}
-        </button>
-    )
-  }
+    function renderizarBotao() {
+        return (
+            <button className={styles.botao}
+                onClick={props.onClick}>
+                {props.texto}
+            </button>
+        )
+    }
 
-  return props.href ? (
-    
-    <Link href={props.href} >
-      {renderizarBotao()}
-    </Link>
-  ) : renderizarBotao()
+    return props.href ? (
+        <Link href={props.href}>
+            {renderizarBotao()}
+        </Link>
+    ) : renderizarBotao()
 }
-
--- components/Questao.tsx
+++ components/Questao.tsx
@@ -1,50 +1,48 @@
-/* eslint-disable react/jsx-key */
-import styles from "../styles/Questao.module.css";
-import QuestaoModel from "../model/questao";
-import Enunciado from "./Enunciado";
-import Temporizador from './Temporizador';
-import Resposta from "./Resposta";
+import styles from '../styles/Questao.module.css'
+import QuestaoModel from '../model/questao'
+import Enunciado from './Enunciado'
+import Temporizador from './Temporizador'
+import Resposta from './Resposta'
 
 const letras = [
-  { valor: 'A', cor: '#f2c866'},
-  { valor: 'B', cor: '#f266ba'},
-  { valor: 'C', cor: '#85d4f2'},
-  { valor: 'D', cor: '#bce596'},
-
+    { valor: 'A', cor: '#F2C866' },
+    { valor: 'B', cor: '#F266BA' },
+    { valor: 'C', cor: '#85D4F2' },
+    { valor: 'D', cor: '#BCE596' },
 ]
 
 interface QuestaoProps {
-  valor: QuestaoModel;
-  tempoPraResposta?: number
-  respostaFornecida: (indice: number) => void
-  tempoEsgotado: () => void
+    valor: QuestaoModel
+    tempoPraResposta?: number
+    respostaFornecida: (indice: number) =>  void
+    tempoEsgotado: () => void
 }
 
 export default function Questao(props: QuestaoProps) {
-  const questao = props.valor;
+    const questao = props.valor
 
-  function renderizarRespostas() {
-    return questao.respostas.map((resposta, i) => {
-      return (
-        <Resposta
-        key={`${questao.id}-${i}`}
-        valor={resposta}
-        indice={i}
-        letra={letras[i].valor}
-        corFundoLetra={letras[i].cor}
-        respostaFornecida={props.respostaFornecida}
-        />
-      );
-    });
-  }
+    function renderizarRespostas() {
+        return questao.respostas.map((resposta, i) => {
+            return (
+                <Resposta
+                    key={`${questao.id}-${i}`}
+                    valor={resposta}
+                    indice={i}
+                    letra={letras[i].valor}
+                    corFundoLetra={letras[i].cor}
+                    respostaFornecida={props.respostaFornecida}
+                />
+            )
+        })
+    }
 
-  return (
-    <div className={styles.questao}>
-      <Enunciado texto={questao.enunciado} />
-      <Temporizador key={questao.id}
-        duracao={props.tempoPraResposta ?? 10}
-        tempoEsgotado={props.tempoEsgotado} />
-      {renderizarRespostas()}
-    </div>
-  )
+    return (
+        <div className={styles.questao}>
+            <Enunciado texto={questao.enunciado} />
+            <Temporizador key={questao.id}
+                duracao={props.tempoPraResposta ?? 10}
+                tempoEsgotado={props.tempoEsgotado} />
+            {renderizarRespostas()}
+        </div>
+    )
 }
