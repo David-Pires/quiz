d84093b416f0474ae87fdd52abddc8dd94e2c494
David Pires <developer.david@outlook.com>
2022-03-28 22:40:19 -0300

    implemented button

---
+++ components/Estatistica.tsx
@@ -0,0 +1,23 @@
+import styles from '../styles/Estatistica.module.css'
+
+interface EstatisticaProps {
+  valor: any
+  texto: string 
+  corFundo?: string 
+  corFonte?: string 
+}
+export default function Estatistica(props: EstatisticaProps) {
+  return (
+    <div className={styles.estatistica}>
+      <div className={styles.valor} style={{
+        backgroundColor: props.corFundo ?? '#fdd60f',
+        color: props.corFonte ??'#333'
+      }}>
+        {props.valor}
+      </div>
+      <div className={styles.texto}> 
+        {props.texto}
+      </div>
+    </div>
+  )
+}
--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,4 +1,6 @@
 import styles from '../styles/Resultado.module.css'
+import Estatistica from '../components/Estatistica'
+import Botao from '../components/Botao'
 import { useRouter } from 'next/router'
 
 export default function resultado() {
@@ -11,9 +13,16 @@ export default function resultado() {
   return (
     <div className={styles.resultado}>
       <h1>Resultado Final</h1>
-      <div>{total}</div>
-      <div>{certas}</div>
-      <div>{`${percentual}%`}</div>
-    </div>
+
+      <div style={{ display: "flex"}}>
+        <Estatistica texto='Perguntas'valor={total} />
+        <Estatistica texto='Certas'valor={certas} 
+           corFundo="#9cd2a4" />
+        <Estatistica texto='Percentual'valor={`${percentual}%`} 
+            corFundo="#de6a33"/>
+        
+      </div>
+      <Botao href="/" texto="Tentar Novamente" /> 
+     </div>
   )
 }
